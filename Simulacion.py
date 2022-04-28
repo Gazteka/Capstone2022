317ebfce63417304bb3b044f3c04aed1992bfbbd
Pablo Acevedo <pablo.acevedo@uc.cl>
2022-04-28 10:29:42 -0400

    primeros pasos simulacion

--- Simulacion.py
+++ Simulacion.py
@@ -4,6 +4,7 @@ from Clases import Sala,Paciente,Hospital
 np.random.seed(222)
 
 def preparar_pacientes(datos_pacientes):
+
     pacientes = datos_pacientes["Case ID"]
     set_pacientes = set(pacientes)
 
@@ -16,60 +17,14 @@ def preparar_pacientes(datos_pacientes):
         estadias = dict.fromkeys(ruta,0)
         for area in ruta:
             estadia = paciente_df[paciente_df["Area"] == area]["Estadia"].iloc[0]
-            estadias[area] = estadia
+            siguiente = paciente_df[paciente_df["Area"] == area]["Siguiente Área"].iloc[0]
+            estadias[area] = (estadia,siguiente)
         estadias["End"] = 0
         paciente_class = Paciente(id,ruta,entrada,estadias)
         pacientes_listos[paciente] = paciente_class
     return pacientes_listos
 
-def cargar_recursos_sala():
-    direccion = os.path.join('Datos', 'recursos_salas.json')
-    dic_salas = dict()
-    with open(direccion) as file:
-        recursos_sala = json.load(file)
-    for nombre_sala in recursos_sala:
-        sala_actual = Sala(nombre_sala)
-        if nombre_sala == 'URG_101_003' or nombre_sala == 'DIV_101_703':
-            sala_actual.recursos['box_atencion'] = dict()
-            for box_atencion in range(1, recursos_sala[nombre_sala]['box_atencion']+1):
-                sala_actual.recursos['box_atencion'][box_atencion] = False
-            sala_actual.recursos['ampliacion_max'] = recursos_sala[nombre_sala]['ampliacion_max']
 
-        elif "DIV" in nombre_sala and nombre_sala != 'DIV_101_703':
-            sala_actual.recursos['camas'] = dict()
-            for cama in range(1, recursos_sala[nombre_sala]['camas']+1):
-                sala_actual.recursos['camas'][cama] = False
-            sala_actual.recursos['ampliacion_max'] = recursos_sala[nombre_sala]['ampliacion_max']
-
-        elif nombre_sala == 'OPR102_001':
-            sala_actual.recursos['quirofanos'] = dict()
-            for quirofano in range(1, recursos_sala[nombre_sala]['quirofanos']+1):
-                sala_actual.recursos['quirofanos'][quirofano] = False
-            sala_actual.recursos['hora_inicio'] = recursos_sala[nombre_sala]['hora_inicio']
-            sala_actual.recursos['hora_final'] = recursos_sala[nombre_sala]['hora_final']
-
-        elif nombre_sala == 'OPR101_011':
-            sala_actual.recursos['quirofanos'] = dict()
-            for quirofano in range(1, recursos_sala[nombre_sala]['quirofanos']+1):
-                sala_actual.recursos['quirofanos'][quirofano] = False
-            sala_actual.recursos['hora_inicio'] = recursos_sala[nombre_sala]['hora_inicio']
-            sala_actual.recursos['hora_final'] = recursos_sala[nombre_sala]['hora_final']
-            sala_actual.recursos['hora_limite'] = recursos_sala[nombre_sala]['hora_limite']
-
-        elif nombre_sala == 'OPR102_003':
-            sala_actual.recursos['hora_inicio'] =  recursos_sala[nombre_sala]['hora_inicio']
-            sala_actual.recursos['hora_final'] = recursos_sala[nombre_sala]['hora_final']
-            sala_actual.recursos['hora_limite'] = recursos_sala[nombre_sala]['hora_limite']
-        
-        elif nombre_sala == 'OPR101_033':
-            sala_actual.recursos['hora_inicio'] = recursos_sala[nombre_sala]['hora_inicio']
-            sala_actual.recursos['hora_final'] = recursos_sala[nombre_sala]['hora_final']
-
-        dic_salas[nombre_sala] = sala_actual
-
-    return dic_salas
-
-cargar_recursos_sala()
 
 dataset,areas = preparar_datos(DIC_DATOS,AREAS)
 
@@ -83,14 +38,20 @@ llegadas = info_pacientes["Entrada"].sort_values()
 
 datos_pacientes = dataset["pacientes"]
 pacientes = preparar_pacientes(datos_pacientes)
+# for paciente in pacientes:
+#     print(pacientes[paciente].estadias)
+
+## Cargamos el hospital
+hospital = Hospital(hospital)
+hospital.recibir_pacientes(pacientes)
+hospital.simular()
+
+# print(hospital)
 
 
-# hospital = Hospital(hospital)
-sala = hospital["URG101_003"]
-for paciente in pacientes:
-    paciente_entra = pacientes[paciente]
-    sala.llegada(paciente_entra)
-    print(sala.pacientes)
-    break
-# print(datos_pacientes.tail(20))
 
+sala = Sala("URG101_003")
+# for paciente in pacientes:
+#     sala.llegada(pacientes[paciente])
+#     sala.salida(pacientes[paciente])
+#     break
